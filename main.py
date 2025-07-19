3c53a2ea06087f3d30d50fdb4ac0cdc30cb8b1c7
ConstantSeal592 <reubensmith@outlook.com>
2025-07-19 15:31:14 +0100

    Start screen
    Start screen added, needs finishing off

--- main.py
+++ main.py
@@ -4,18 +4,23 @@ from fileInteraction import LoadEndings
 
 NAME = "HEARTSTRING HIGH"
 
-def typeWrite(message, addNewLine=True):
-    for char in message:
+def typeWrite(message, addNewLine=True, endOfLinePause = 0.75):
+    for i,char in enumerate(message):
         print(char, end='', flush=True)
-        if not char in [' ', '\n']:
-            time.sleep(random.randint(2,14)/100)
+        if char == '.':
+            section = message[i-2:i+3]
+            if section.count('.') >= 3:
+                time.sleep(0.75)
+        elif not char in [' ', '\n']:
+            time.sleep(random.randint(3,14)/100)
 
     if addNewLine == True:
+        time.sleep(endOfLinePause)
         print()
 
 def getNumberInput(Min, Max):
     while True:
-        typeWrite("--> ", False)
+        typeWrite("--> ", False, 0)
         userInput = input()
         print()
 
@@ -29,18 +34,34 @@ def getNumberInput(Min, Max):
             typeWrite("Please Enter an integer number")
 
 def typeAskSelection(prompt, *args):
-    typeWrite(prompt)
+    typeWrite(prompt, True, 0)
     print()
     for i, choice in enumerate(args):
-        typeWrite(f"{i+1}) {choice}")
+        typeWrite(f"{i+1}) {choice}", True, 0)
     
     number = getNumberInput(1, len(args))
     print()
     return number
 
 def typeAskNum(prompt, Min=0, Max=4):
-    typeWrite(prompt)
+    typeWrite(prompt, True, 0)
     return getNumberInput(Min, Max)
+
+def newGameDialogue():
+    typeWrite("Xingping, Yangshuo, China | 05/02/25 - 20:10 UTC+8")
+    typeWrite("Chuhao walks along a dimmly lit allyway... 2 shadowy figures follow closely behind.")
+    typeWrite("He walks around a corner onto a wider street where the dazzling white lights of a Ford Transit T-350 blind him.")
+    typeWrite("The vehicle doesnt slow for the frozen Chuhoa as the 3.5 tonne van slams into his body.")
+    typeWrite("...")
+    typeWrite("The two figures move out of the shadows to quickly and efficiently move his chinese body into the back of the now blood stained van.")
+    time.sleep(3)
+    print()
+    print()
+    print()
+    typeWrite("Caistor, Lincolnshire, England | 06/02/25 - 08:45 UTC+0")
+    typeWrite("Chuhao awakes slowly tied to a chair in a clean tidy office. On the wall, hangs a clock and several peices of artwork.")
+    typeWrite("Suddenly the door slams open and an angry scottish woman enters accompanied with a small bedraggled man dressed in all black.")
+    typeWrite("The woman takes her place behind the desk infront of Chuhao glaring intensly at him")
 
 SQUARESIDES = 5
 def displayEndings():
@@ -71,13 +92,12 @@ def main():
             '#'*40
         ]))
     
-    choice = 3
-    # choice = typeAskSelection("Choose Your Action:",
-    #                           "Start new game",
-    #                           "Load game",
-    #                           "View endings")
+    choice = typeAskSelection("Choose Your Action:",
+                              "Start new game",
+                              "Load game",
+                              "View endings")
     if choice == 1:
-        pass
+        newGameDialogue()
     elif choice == 2:
         pass
     elif choice == 3:
